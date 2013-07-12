1c4d8b9408b6c0c7799225dfa732ee8f1fc92905
Gustavo Andriotti <gustavo.andriotti@hp.com>
2013-07-12 13:32:54 -0300

    Added basic Devise User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,16 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :password_confirmation
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :name, :password, :password_confirmation, :remember_me
+  #attr_accessible :email, :name, :password, :password_confirmation
+  #has_secure_password
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
