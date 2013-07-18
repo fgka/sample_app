b76ed5a4c27057ccde93d2c53e9dd76847dc94a7
Gustavo Andriotti <gustavo.andriotti@hp.com>
2013-07-18 16:02:04 -0300

    Added proper tenant setup to factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,17 +1,34 @@
+module FactoryGirl
+  module TenantHelper    
+    def self.current_tenant()
+      return Thread.current[:tenant_id]
+    end
+  end
+end
+
 FactoryGirl.define do
+
+  factory :tenant do
+    sequence(:name) { |n| "Tenant #{n}"}
+    sequence(:id) { |n| n }
+    tenant_id nil
+  end
+
   factory :user do
     sequence(:name)  { |n| "Person #{n}" }
     sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
+    tenant_id nil
 
     factory :admin do
       admin true
     end
   end
-  
+
   factory :micropost do
     content "Lorem ipsum"
     user
+    tenant_id FactoryGirl::TenantHelper::current_tenant
   end
 end
