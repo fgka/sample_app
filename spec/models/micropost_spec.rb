e7e54bb645ba08bff234cadadd9594ed33cc69e4
Gustavo Andriotti <gustavo.andriotti@hp.com>
2013-07-18 16:17:06 -0300

    micropost passing all tests

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -2,14 +2,21 @@ require 'spec_helper'
 
 describe Micropost do
 
+  let(:tenant) { FactoryGirl.create(:tenant) }
   let(:user) { FactoryGirl.create(:user) }
-  before { @micropost = user.microposts.build(content: "Lorem ipsum") }
+  before do
+    set_tenant tenant
+    @micropost = user.microposts.build(content: "Lorem ipsum")
+  end
 
   subject { @micropost }
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
+  it { should_not allow_mass_assignment_of(:user_id) }
   it { should respond_to(:user) }
+  it { should respond_to(:tenant) }
+  it { should_not allow_mass_assignment_of(:tenant_id) }
   its(:user) { should == user }
 
   it { should be_valid }
@@ -17,14 +24,6 @@ describe Micropost do
   describe "when user_id is not present" do
     before { @micropost.user_id = nil }
     it { should_not be_valid }
-  end
-
-  describe "accessible attributes" do
-    it "should not allow access to user_id" do
-      expect do
-        Micropost.new(user_id: user.id)
-      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
-    end
   end
 
   describe "with blank content" do
@@ -36,5 +35,4 @@ describe Micropost do
     before { @micropost.content = "a" * 141 }
     it { should_not be_valid }
   end
-
 end
