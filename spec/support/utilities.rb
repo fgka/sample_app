1f68d2faab3532a116edb0cae5b79ae7d28b7aa5
Gustavo Andriotti <gustavo.andriotti@hp.com>
2013-07-18 16:02:42 -0300

    Added proper tenant setup to all tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -13,8 +13,8 @@ RSpec::Matchers.define :have_error_message do |message|
 end
 
 def sign_in(user)
-  begin 
-    find_link("Sign out").click    
+  begin
+    find_link("Sign out").click
   rescue
   end
   visit new_user_session_path
@@ -22,3 +22,20 @@ def sign_in(user)
   fill_in "user_password", with: user.password
   click_button "Sign in"
 end
+
+def set_tenant( tenant )
+  Thread.current[:tenant_id]  = tenant.id
+end
+
+def current_tenant()
+  return Thread.current[:tenant_id]
+end
+
+def reset_tenant()
+  Thread.current[:tenant_id]  = nil   # starting point; no tenant
+end
+
+def void_tenant()
+  Thread.current[:tenant_id]  = 0   # an impossible tenant
+end
+
