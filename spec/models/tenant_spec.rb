cef8ac8c36fe751b5122b17f6cd55ba27c5ed539
Gustavo Andriotti <gustavo.andriotti@hp.com>
2013-07-18 15:26:01 -0300

    Trying to adequate factory to generate tenant based entities

--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Tenant do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @tenant = Tenant.create_new_tenant({name: "Example Tenant"}) }
+
+  subject { @tenant }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:tenant) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @tenant.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too short" do
+    before { @tenant.name = "a" * 2 }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @tenant.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+  describe "when name is not unique" do
+    expect { Tenant.create_new_tenant({name: @tenant.name}) }.to raise_error
+  end
 end
