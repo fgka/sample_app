3eb053ee8646db7641ee62be208626a253f516e7
Gustavo Andriotti <gustavo.andriotti@hp.com>
2013-07-26 17:06:02 -0300

    Added tenant ref

--- db/migrate/20130726200445_add_tenant_to_microposts.rb
+++ db/migrate/20130726200445_add_tenant_to_microposts.rb
@@ -1,4 +1,8 @@
 class AddTenantToMicroposts < ActiveRecord::Migration
   def change
+    change_table(:microposts) do |t|
+      t.references :tenant
+    end
+    add_index :microposts, :tenant_id
   end
 end
