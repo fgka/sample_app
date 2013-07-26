17d1285f4fae60ca6ff6c24d8c36955a6bf60d6e
Gustavo Andriotti <gustavo.andriotti@hp.com>
2013-07-26 17:07:33 -0300

    added tenant to relationships

--- db/migrate/20130726200636_add_tenant_to_relationships.rb
+++ db/migrate/20130726200636_add_tenant_to_relationships.rb
@@ -1,4 +1,8 @@
 class AddTenantToRelationships < ActiveRecord::Migration
   def change
+    change_table(:relationships) do |t|
+      t.references :tenant
+    end
+    add_index :relationships, :tenant_id
   end
 end
