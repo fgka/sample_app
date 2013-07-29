74c5b708112aec9a4c0f6a4e54eb3d2d92096585
Gustavo Andriotti <gustavo.andriotti@hp.com>
2013-07-29 16:52:39 -0300

    Model Tenant passing

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -4,4 +4,10 @@ class Tenant < ActiveRecord::Base
   has_and_belongs_to_many :users
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: { case_sensitive: false }
+
+  def self.create_new_tenant(params)
+    tenant = Tenant.create!(name: params[:name])
+
+    return tenant
+  end
 end
--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Tenant do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @tenant = FactoryGirl.create(:tenant) }
+
+  subject { @tenant }
+
+  it { should respond_to(:name) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @tenant.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too short" do
+    before { @tenant.name = "a" * 2 }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @tenant.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+  describe "when name is not unique" do
+    it "should raise" do
+      expect { Tenant.create_new_tenant({name: @tenant.name}) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
