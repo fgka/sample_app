28fab107258e8d0a513ac3a39d495cb0e683b4e8
Gustavo Andriotti <gustavo.andriotti@hp.com>
2013-07-29 17:57:27 -0300

    Added DB clean for all tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,7 +31,17 @@ Spork.prefork do
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
     # instead of true.
-    config.use_transactional_fixtures = true
+    # config.use_transactional_fixtures = true
+    config.use_transactional_fixtures = false
+
+    config.before :each do
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.start
+    end
+
+    config.after do
+      DatabaseCleaner.clean
+    end
 
     # If true, the base class of anonymous controllers will be inferred
     # automatically. This will be the default behavior in future versions of
