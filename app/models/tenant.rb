9c8bdaab6b5ed23605dee4e056cbdb4f27c92907
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-07-30 19:07:08 -0300

    Tenant model working with monkey patch

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,7 +1,7 @@
 class Tenant < ActiveRecord::Base
   attr_accessible :name
 
-  has_and_belongs_to_many :users
+  acts_as_tenant
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: { case_sensitive: false }
 
