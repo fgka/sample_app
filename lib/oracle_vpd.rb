60ff660fc822b8a99f31bef77ed17a30bd99204c
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-08-01 16:54:34 -0300

    Now we have some progress on the patch

--- lib/oracle_vpd.rb
+++ lib/oracle_vpd.rb
@@ -1,5 +1,9 @@
+require 'rails'
 require 'oracle_vpd/base'
 require 'oracle_vpd/control'
 require 'oracle_vpd/connection_pool'
 
-require 'oracle_vpd/railtie' if defined?(Rails::Railtie)
+Rails.configuration.to_prepare do
+  ActiveRecord::Base.send(:include, OracleVPD::Base)
+  ActionController::Base.send(:include, OracleVPD::Control)
+end
--- lib/oracle_vpd/connection_pool.rb
+++ lib/oracle_vpd/connection_pool.rb
@@ -2,12 +2,29 @@ module ActiveRecord::ConnectionAdapters
   class ConnectionPool
 
     alias_method :old_checkout, :checkout
+
     def new_checkout
-      msg = "CONN OK, worked"
+      conn = old_checkout
+      msg = "CONN CHECKOUT: after"
       Rails.logger.info msg
       puts msg
-      old_checkout
+
+      conn
     end
+
+    alias_method :checkout, :new_checkout
+
+    alias_method :old_checkin, :checkin
+
+    def new_checkin(conn)
+      msg = "CONN CHECKIN: before"
+      Rails.logger.info msg
+      puts msg
+
+      old_checkin conn
+    end
+
+    alias_method :checkin, :new_checkin
   end
 end
 
