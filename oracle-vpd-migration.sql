64f437e44d84017bdb0dbf1cec1d2a80ab778d38
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-08-01 21:54:12 -0300

    Changed the package to use functions instead of procedures so that a select from dual works

--- oracle-vpd-migration.sql
+++ oracle-vpd-migration.sql
@@ -11,20 +11,28 @@ CREATE OR REPLACE CONTEXT tenant_ctx USING tenant_ctx_pkg
 -- To remove the package:
 --  DROP PACKAGE tenant_ctx_pkg;
 CREATE OR REPLACE PACKAGE tenant_ctx_pkg IS
- PROCEDURE set_tenant_id(tenant_id IN NUMBER);
- PROCEDURE clear_tenant_id;
+ FUNCTION set_tenant_id(tenant_id IN NUMBER) RETURN NUMBER;
+ FUNCTION clear_tenant_id RETURN NUMBER;
  END;
 /
 CREATE OR REPLACE PACKAGE BODY tenant_ctx_pkg IS
-  PROCEDURE set_tenant_id(
-    tenant_id IN NUMBER
-  ) IS
+  ret_tenant_id NUMBER := -1;
+  FUNCTION set_tenant_id(
+    tenant_id IN NUMBER)
+  RETURN NUMBER
+  IS
   BEGIN
-     DBMS_SESSION.SET_CONTEXT('tenant_ctx', 'tenant_id', tenant_id);
+    DBMS_SESSION.SET_CONTEXT('tenant_ctx', 'tenant_id', tenant_id);
+    SELECT SYS_CONTEXT('tenant_ctx', 'tenant_id') INTO ret_tenant_id FROM dual;
+    RETURN ret_tenant_id;
   END set_tenant_id;
-  PROCEDURE clear_tenant_id IS
+  FUNCTION clear_tenant_id
+  RETURN NUMBER
+  IS
   BEGIN
-     DBMS_SESSION.SET_CONTEXT('tenant_ctx', 'tenant_id', NULL);
+      SELECT SYS_CONTEXT('tenant_ctx', 'tenant_id') INTO ret_tenant_id FROM dual;
+      DBMS_SESSION.SET_CONTEXT('tenant_ctx', 'tenant_id', NULL);
+      RETURN ret_tenant_id;
   END clear_tenant_id;
 END;
 /
@@ -66,11 +74,11 @@ END;
 /
 
 -- Test:
--- EXECUTE TENANT_CTX_PKG.SET_TENANT_ID(1);
+-- EXECUTE tenant_ctx_pkg.set_tenant_id(1);
 -- SELECT * FROM my_table; -- just the row starting with 1234
--- EXECUTE TENANT_CTX_PKG.SET_TENANT_ID(2);
+-- EXECUTE tenant_ctx_pkg.set_tenant_id(2);
 -- SELECT * FROM my_table; -- just the row starting with 4321
--- EXECUTE TENANT_CTX_PKG.CLEAR_TENANT_ID;
+-- EXECUTE tenant_ctx_pkg.clear_tenant_id;
 -- SELECT * FROM my_table; -- should return no rows
 
 --
