6dc67797a5f70f320733a31b3e3fd3afe208d18a
Gustavo Andriotti <gustavo.andriotti@hp.com>
2013-08-05 18:00:33 -0300

    Added the a function that returns the content for the variable @tenant_id

--- db/migrate/20130805204452_create_tenant_getter_function.rb
+++ db/migrate/20130805204452_create_tenant_getter_function.rb
@@ -1,7 +1,21 @@
 class CreateTenantGetterFunction < ActiveRecord::Migration
   def up
+    execute <<-SQL
+    DROP FUNCTION IF EXISTS get_ctx_tenant;
+    SQL
+    execute <<-SQL
+    CREATE FUNCTION get_ctx_tenant()
+      RETURNS INTEGER
+      LANGUAGE SQL
+    BEGIN
+      RETURN @tenant_id;
+    END;
+    SQL
   end
 
   def down
+    execute <<-SQL
+      DROP FUNCTION IF EXISTS get_ctx_tenant;
+    SQL
   end
 end
