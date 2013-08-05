77780464354c4796cfe1abd965eb6e5fe6a52b18
Gustavo Andriotti <gustavo.andriotti@hp.com>
2013-08-05 18:00:56 -0300

    Added proper permissions for users to create views and functions

--- mysql_scripts/create_db.sql
+++ mysql_scripts/create_db.sql
@@ -3,19 +3,28 @@
 -- user: rubydev
 -- pass: rubydev
 CREATE DATABASE sampleapp_dev CHARACTER SET utf8;
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER,INDEX,LOCK TABLES ON sampleapp_dev.* TO 'rubydev'@'localhost' IDENTIFIED BY 'rubydev';
+GRANT
+    SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX, LOCK TABLES,
+    CREATE VIEW, SHOW VIEW, ALTER ROUTINE, CREATE ROUTINE, EXECUTE
+        ON sampleapp_dev.* TO 'rubydev'@'localhost' IDENTIFIED BY 'rubydev';
 FLUSH PRIVILEGES;
 -- Test:
 -- sampleapp_tst
 -- user: rubyts
 -- pass: rubytst
 CREATE DATABASE sampleapp_tst CHARACTER SET utf8;
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER,INDEX,LOCK TABLES ON sampleapp_tst.* TO 'rubytst'@'localhost' IDENTIFIED BY 'rubytst';
+GRANT
+    SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX, LOCK TABLES,
+    CREATE VIEW, SHOW VIEW, ALTER ROUTINE, CREATE ROUTINE, EXECUTE
+        ON sampleapp_tst.* TO 'rubytst'@'localhost' IDENTIFIED BY 'rubytst';
 FLUSH PRIVILEGES;
 -- Production:
 -- sampleapp_prd
 -- user: rubyprd
 -- pass: rubyprd
 CREATE DATABASE sampleapp_prd CHARACTER SET utf8;
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER,INDEX,LOCK TABLES ON sampleapp_prd.* TO 'rubyprd'@'localhost' IDENTIFIED BY 'rubyprd';
+GRANT
+    SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX, LOCK TABLES,
+    CREATE VIEW, SHOW VIEW, ALTER ROUTINE, CREATE ROUTINE, EXECUTE
+        ON sampleapp_prd.* TO 'rubyprd'@'localhost' IDENTIFIED BY 'rubyprd';
 FLUSH PRIVILEGES;
