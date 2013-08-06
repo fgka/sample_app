8fb3610695920f4587f9d9ed5febb110feda519f
Gustavo Andriotti <gustavo.andriotti@hp.com>
2013-08-06 15:45:49 -0300

    Added new file to requirements

--- lib/mysql_vpd.rb
+++ lib/mysql_vpd.rb
@@ -1,9 +1,11 @@
 require 'rails'
 require 'mysql_vpd/base'
 require 'mysql_vpd/control'
+require 'mysql_vpd/migration'
 require 'mysql_vpd/connection_pool'
 
 Rails.configuration.to_prepare do
   ActiveRecord::Base.send(:include, MysqlVPD::Base)
   ActionController::Base.send(:include, MysqlVPD::Control)
+  ActiveRecord::Migration.send(:include, MysqlVPD::Migration)
 end
