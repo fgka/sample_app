f9767ae98deb32bad6f26d8731266835ca3883f7
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-08-11 16:45:58 -0300

    Fully automated DB creation

--- mysql_scripts/create_db.sql
+++ mysql_scripts/create_db.sql
@@ -1,7 +1,11 @@
+-- To create the databases, execute the following:
+--    mysql -u root -p < create_db.sql
+--
 -- Development:
 -- sampleapp_dev
 -- user: rubydev
 -- pass: rubydev
+DROP DATABASE IF EXISTS sampleapp_dev;
 CREATE DATABASE sampleapp_dev CHARACTER SET utf8;
 GRANT
     SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX, LOCK TABLES,
@@ -12,6 +16,7 @@ FLUSH PRIVILEGES;
 -- sampleapp_tst
 -- user: rubyts
 -- pass: rubytst
+DROP DATABASE IF EXISTS sampleapp_tst;
 CREATE DATABASE sampleapp_tst CHARACTER SET utf8;
 GRANT
     SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX, LOCK TABLES,
@@ -22,9 +27,14 @@ FLUSH PRIVILEGES;
 -- sampleapp_prd
 -- user: rubyprd
 -- pass: rubyprd
+DROP DATABASE IF EXISTS sampleapp_prd;
 CREATE DATABASE sampleapp_prd CHARACTER SET utf8;
 GRANT
     SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX, LOCK TABLES,
     CREATE VIEW, SHOW VIEW, ALTER ROUTINE, CREATE ROUTINE, EXECUTE, TRIGGER
         ON sampleapp_prd.* TO 'rubyprd'@'localhost' IDENTIFIED BY 'rubyprd';
 FLUSH PRIVILEGES;
+-- Just checking
+USE mysql;
+SELECT db, user FROM db where db LIKE 'sampleapp_%';
+SELECT user, host FROM user WHERE user LIKE 'ruby%';
