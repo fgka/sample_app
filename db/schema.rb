74e5169c76869d826eb1eead337f9bb15c49f177
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-09-04 17:14:39 -0300

    redoing schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,11 @@
 
 ActiveRecord::Schema.define(:version => 20130811200746) do
 
-  create_table "microposts", :id => false, :force => true do |t|
-    t.integer  "id",         :default => 0, :null => false
+  create_table "microposts", :force => true do |t|
     t.string   "content"
     t.integer  "user_id"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "mt_microposts", :force => true do |t|
@@ -43,12 +42,11 @@ ActiveRecord::Schema.define(:version => 20130811200746) do
   add_index "mt_relationships", ["follower_id", "followed_id"], :name => "index_mt_relationships_on_follower_id_and_followed_id", :unique => true
   add_index "mt_relationships", ["follower_id"], :name => "index_mt_relationships_on_follower_id"
 
-  create_table "relationships", :id => false, :force => true do |t|
-    t.integer  "id",          :default => 0, :null => false
+  create_table "relationships", :force => true do |t|
     t.integer  "follower_id"
     t.integer  "followed_id"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "sessions", :force => true do |t|
