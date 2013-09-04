0794808a6ecbd8aaea1ae929e8939ae78d183094
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-09-04 17:14:25 -0300

    Removed useless comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,29 +5,22 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   before_filter :authenticate_user!
-  before_filter :set_current_tenant # forces milia to set up current tenant
+  before_filter :set_current_tenant
 
   def authenticate_tenant!()
 
     unless authenticate_user!
       email = ( params.nil? || params[:user].nil?  ?  ""  : " as: " + params[:user][:email] )
-
       flash[:notice] = "cannot sign you in#{email}; check email/password and try again"
-
-      return false  # abort the before_filter chain
+      return false
     end
 
-    # user_signed_in? == true also means current_user returns valid user
     raise SecurityError,"*** invalid sign-in  ***" unless user_signed_in?
 
-    set_current_tenant   # relies on current_user being non-nil
-
-    # any application-specific environment set up goes here
-
-    true  # allows before filter chain to continue
+    set_current_tenant
+    true
   end
 
-  # Force signout to prevent CSRF attacks
   def handle_unverified_request
     sign_out
     super
